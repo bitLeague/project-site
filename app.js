e9b9482f02edd9dd2397063b22d75647ac31d8ab
Tom Prince <tprince14@gmail.com>
2017-04-03 20:04:05 -0400

    remove keystone and check env for port

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+let port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 })
