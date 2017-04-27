ac9a2008bb4ced1cfcb1621bc5323fa3629499d1
Tom Prince <tprince14@gmail.com>
2017-04-27 10:49:58 -0500

    refactor

--- public/app/index.js
+++ public/app/index.js
@@ -1,79 +1,77 @@
-var app = angular.module('myApp', [
-    /*
-     * Order is not important. Angular makes a
-     * pass to register all of the modules listed
-     * and then when app.dashboard tries to use app.data,
-     * its components are available.
-     */
+(function() {
+    'use strict';
+    angular.module('myApp', [
+        /*
+         * Order is not important. Angular makes a
+         * pass to register all of the modules listed
+         * and then when app.dashboard tries to use app.data,
+         * its components are available.
+         */
 
-    /*
-     * Everybody has access to these.
-     * We could place these under every feature area,
-     * but this is easier to maintain.
-     */
-    'myApp.core',
-    /*
-     * Feature areas
-     */
-    'myApp.dashboard'
-]);
+        /*
+         * Everybody has access to these.
+         * We could place these under every feature area,
+         * but this is easier to maintain.
+         */
+        'myApp.core',
+        /*
+         * Feature areas
+         */
+        'myApp.dashboard'
+    ])
+    .factory('userService', function() {
+        var userData = {};
 
+        function set(data) {
+            userData = data;
+        }
 
+        function get() {
+            return userData;
+        }
 
+        return {
+            set: set,
+            get: get
+        }
+    })
+    .factory('tickerService', function() {
+        var tickerData = {};
 
-app.factory('userService', function() {
-    var userData = {};
+        function set(data) {
+            tickerData = data;
+        }
 
-    function set(data) {
-        userData = data;
-    }
+        function get() {
+            return tickerData;
+        }
 
-    function get() {
-        return userData;
-    }
+        return {
+            set: set,
+            get: get
+        }
+    })
 
-    return {
-        set: set,
-        get: get
-    }
-});
+    .factory('reportService', function() {
+        var reportName;
+        var reportData = [];
 
-app.factory('tickerService', function() {
-    var tickerData = {};
+        function set(name, data) {
+            reportName = name;
+            reportData = data;
+        }
 
-    function set(data) {
-        tickerData = data;
-    }
+        function getData() {
+            return reportData;
+        }
 
-    function get() {
-        return tickerData;
-    }
-
-    return {
-        set: set,
-        get: get
-    }
-});
-
-app.factory('reportService', function() {
-    var reportName;
-    var reportData = [];
-
-    function set(name, data) {
-        reportName = name;
-        reportData = data;
-    }
-
-    function getData() {
-        return reportData;
-    }
-
-    function getName() {
-        return reportName;
-    }
-    return {
-        set: set,
-        getData: getData,
-        getName: getName
-    }
-});
+        function getName() {
+            return reportName;
+        }
+        return {
+            set: set,
+            getData: getData,
+            getName: getName
+        }
+    });
+})();
